811a597044c97f10888f54da217ced4f8d5f967a
ZTGP_VR <popov-mp@mail.ru>
2024-04-10 12:47:17 +0500

    Изменение 1

---
+++ src/main/java/org/example/lesson1/HomeWork1.java
@@ -0,0 +1,16 @@
+package org.example.lesson1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+    int a = 78;
+    do
+
+    {
+        a++;
+        System.out.println(a);
+    }
+    while(a<100);
+}
+
+
+}
