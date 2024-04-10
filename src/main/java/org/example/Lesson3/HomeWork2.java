b46d404b1625af0c5febbf34cd79c572bcae21f8
ZTGP_VR <popov-mp@mail.ru>
2024-04-10 13:48:43 +0500

    Комит 2

--- src/main/java/org/example/Lesson3/HomeWork2.java
+++ src/main/java/org/example/Lesson3/HomeWork2.java
@@ -3,5 +3,6 @@ package org.example.Lesson3;
 public class HomeWork2 {
     public static void main(String[] args) {
     System.out.println("Задание №2");
+    System.out.println("Комит 2");
     }
 }
